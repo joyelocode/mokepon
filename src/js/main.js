462c9d41609f19cad3db49b36400b7e29f26d27a
joyelo <joyelo232018@gmail.com>
2023-09-21 00:11:08 -0300

    Creando el movimiento continuo en todas las direcciones

--- src/js/main.js
+++ src/js/main.js
@@ -40,6 +40,7 @@ let victoriasEnemigo = 0;
 let vidasJugador = 3;
 let vidasEnemigo = 3;
 let lienzo = mapa.getContext('2d');
+let intervalo;
 
 class Mokepon {
     constructor(nombre, foto, vida) {
@@ -53,6 +54,8 @@ class Mokepon {
         this.alto = 80;
         this.mapaFoto = new Image();
         this.mapaFoto.src = foto;
+        this.velocidadX = 0;
+        this.velocidadY = 0;
     };
 };
 
@@ -126,6 +129,7 @@ function seleccionarMascotaJugador() {
     //sectionSeleccionarAtaque.style.display = 'flex';
 
     sectionVerMapa.style.display = 'flex';
+    intervalo = setInterval(pintarPersonaje, 50);
     
     
 
@@ -306,6 +310,8 @@ function aleatorio(min, max) {
 };
 
 function pintarPersonaje() {
+    capipepo.x = capipepo.x + capipepo.velocidadX;
+    capipepo.y = capipepo.y + capipepo.velocidadY;
     lienzo.clearRect(0, 0, mapa.width, mapa.height);
     lienzo.drawImage(
         capipepo.mapaFoto,
@@ -316,9 +322,21 @@ function pintarPersonaje() {
     );
 };
 
-function moverCapipepo() {
-    capipepo.x = capipepo.x + 5
-    pintarPersonaje();
+function moverDerecha() {
+    capipepo.velocidadX = 5;
+};
+function moverIzquierda() {
+    capipepo.velocidadX = -5;
+};
+function moverAbajo() {
+    capipepo.velocidadY = 5;
+};
+function moverArriba() {
+    capipepo.velocidadY = -5;
+};
+function detenerMovimiento() {
+    capipepo.velocidadX = 0;
+    capipepo.velocidadY = 0;
 }
 
 window.addEventListener('load', iniciarJuego);
