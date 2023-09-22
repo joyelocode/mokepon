272f0f6ed9e0e13854391d12ebd4471ff8b809f1
joyelo <joyelo232018@gmail.com>
2023-09-21 22:20:26 -0300

    Obstaculos y colisiones

--- src/js/main.js
+++ src/js/main.js
@@ -347,6 +347,12 @@ function pintarCanvas() {
     hipodogeEnemigo.pintarMokepon();
     capipepoEnemigo.pintarMokepon();
     ratigueyaEnemigo.pintarMokepon();
+
+    if (mascotaJugadorObjeto.velocidadX !== 0 || mascotaJugadorObjeto.velocidadY !== 0) {
+        revisarColision(hipodogeEnemigo);
+        revisarColision(capipepoEnemigo);
+        revisarColision(ratigueyaEnemigo);
+    };
 };
 
 function moverDerecha() {
@@ -402,7 +408,31 @@ function obtenerObjetoMascota() {
             return mokepones[i];
         };  
     };
-}
+};
+
+function revisarColision(enemigo) {
+    const arribaEnemigo = enemigo.y;
+    const abajoEnemigo = enemigo.y + enemigo.alto;
+    const derechaEnemigo = enemigo.x + enemigo.ancho;
+    const izquierdaEnemigo = enemigo.x;
+
+    const arribaMascota = mascotaJugadorObjeto.y;
+    const abajoMascota = mascotaJugadorObjeto.y + enemigo.alto;
+    const derechaMascota = mascotaJugadorObjeto.x + enemigo.ancho;
+    const izquierdaMascota = mascotaJugadorObjeto.x;
+
+    if (
+        abajoMascota < arribaEnemigo ||
+        arribaMascota > abajoEnemigo ||
+        derechaMascota < izquierdaEnemigo ||
+        izquierdaMascota > derechaEnemigo
+    ) {
+        return;
+    };
+
+    detenerMovimiento();
+    alert('Hay Colision ' + enemigo.nombre);
+};
 
 window.addEventListener('load', iniciarJuego);
 
