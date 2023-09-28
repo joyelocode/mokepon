d6eb4a39ae8988eb78e24572b122d639af79ba15
joyelo <joyelo232018@gmail.com>
2023-09-27 22:14:27 -0300

    Mapa responsivo y ubicacion aleatoria de las mascotas en el mapa

--- src/js/main.js
+++ src/js/main.js
@@ -44,17 +44,29 @@ let lienzo = mapa.getContext('2d');
 let intervalo;
 let mapaBackground = new Image();
 mapaBackground.src = '//wsl.localhost/Ubuntu/home/joyelo/developer/cursos/programacion-basica/mokepon/src/assets/img/mokemap.png';
+let alturaQueBuscamos 
+let anchoDelMapa = window.innerWidth - 20;
+const anchaMaximoDelMapa = 350;
+
+if (anchoDelMapa > anchaMaximoDelMapa) {
+    anchoDelMapa = anchaMaximoDelMapa - 20;
+};
+
+alturaQueBuscamos = anchoDelMapa * 600 / 800;
+
+mapa.width = anchoDelMapa;
+mapa.height = alturaQueBuscamos;
 
 class Mokepon {
-    constructor(nombre, foto, vida, fotoMapa, x = 10, y = 10) {
+    constructor(nombre, foto, vida, fotoMapa) {
         this.nombre = nombre;
         this.foto = foto;
         this.vida = vida;
         this.ataques = [];
-        this.x = x;
-        this.y = y;
         this.ancho = 40;
         this.alto = 40;
+        this.x = aleatorio(0, mapa.width - this.ancho);
+        this.y = aleatorio(0, mapa.height - this.alto);
         this.mapaFoto = new Image();
         this.mapaFoto.src = fotoMapa;
         this.velocidadX = 0;
@@ -78,11 +90,11 @@ let capipepo = new Mokepon('Capipepo', './src/assets/img/mokepons_mokepon_capipe
 
 let ratigueya = new Mokepon('Ratigueya', './src/assets/img/mokepons_mokepon_ratigueya_attack.png', 5, '//wsl.localhost/Ubuntu/home/joyelo/developer/cursos/programacion-basica/mokepon/src/assets/img/ratigueya.png');
 
-let hipodogeEnemigo = new Mokepon('Hipodoge', './src/assets/img/mokepons_mokepon_hipodoge_attack.png', 5, '//wsl.localhost/Ubuntu/home/joyelo/developer/cursos/programacion-basica/mokepon/src/assets/img/hipodoge.png', 80, 120);
+let hipodogeEnemigo = new Mokepon('Hipodoge', './src/assets/img/mokepons_mokepon_hipodoge_attack.png', 5, '//wsl.localhost/Ubuntu/home/joyelo/developer/cursos/programacion-basica/mokepon/src/assets/img/hipodoge.png');
 
-let capipepoEnemigo = new Mokepon('Capipepo', './src/assets/img/mokepons_mokepon_capipepo_attack.png', 5, '//wsl.localhost/Ubuntu/home/joyelo/developer/cursos/programacion-basica/mokepon/src/assets/img/capipepo.png', 150, 95);
+let capipepoEnemigo = new Mokepon('Capipepo', './src/assets/img/mokepons_mokepon_capipepo_attack.png', 5, '//wsl.localhost/Ubuntu/home/joyelo/developer/cursos/programacion-basica/mokepon/src/assets/img/capipepo.png');
 
-let ratigueyaEnemigo = new Mokepon('Ratigueya', './src/assets/img/mokepons_mokepon_ratigueya_attack.png', 5, '//wsl.localhost/Ubuntu/home/joyelo/developer/cursos/programacion-basica/mokepon/src/assets/img/ratigueya.png', 200, 190);
+let ratigueyaEnemigo = new Mokepon('Ratigueya', './src/assets/img/mokepons_mokepon_ratigueya_attack.png', 5, '//wsl.localhost/Ubuntu/home/joyelo/developer/cursos/programacion-basica/mokepon/src/assets/img/ratigueya.png');
 
 hipodoge.ataques.push(
     { nombre: '💧', id: 'boton-agua' },
@@ -180,8 +192,10 @@ function seleccionarMascotaJugador() {
         mascotaJugador = inputRatigueya.id;
     } else {
         alert('Selecciona una mascota');
+        return;
     };
 
+    
     extraerAtaques(mascotaJugador);
 
     sectionVerMapa.style.display = 'flex';
@@ -242,13 +256,11 @@ function secuenciaAtaque() {
     
 };
 
-function seleccionarMascotaEnemigo() {
+function seleccionarMascotaEnemigo(enemigo) {
     
-    let mascotaAleatorio = aleatorio(0, mokepones.length -1);
-    
-    spanMascotaEnemigo.innerHTML = mokepones[mascotaAleatorio].nombre;
-    ataquesMokeponEnemigo = mokepones[mascotaAleatorio].ataques;
-
+    spanMascotaEnemigo.innerHTML = enemigo.nombre
+    ataquesMokeponEnemigo = enemigo.ataques
+   
     secuenciaAtaque();
 };
 
@@ -407,8 +419,7 @@ function SePresionoUnaTecla(event) {
 };
 
 function iniciarMapa() {
-    mapa.width = 320;
-    mapa.height = 240;
+
     mascotaJugadorObjeto = obtenerObjetoMascota(mascotaJugador);
 
     intervalo = setInterval(pintarCanvas, 50);
@@ -451,6 +462,7 @@ function revisarColision(enemigo) {
     sectionSeleccionarAtaque.style.display = 'flex';
     sectionVerMapa.style.display = 'none';
     seleccionarMascotaEnemigo(enemigo);
+    
 };
 
 window.addEventListener('load', iniciarJuego);
