ec111ffb137b4525d8903760897ae8af3e85f2e3
joyelo <joyelo232018@gmail.com>
2023-09-27 23:49:36 -0300

    Preparando todo lo necesario para trabajar el backend del juego

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hola Mundo')
+})
+
+app.listen(8080, () => {
+    console.log('servidor funcionando');
+});
